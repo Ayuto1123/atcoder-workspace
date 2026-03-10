fffe71e5cf9fd69e9959fc8c579c74a44308052e
a.yuto1555@gmail.com <ayuto@nextreme.localdomain>
2026-03-10 20:40:34 +0900

    Solved: abc300_b at 2026-03-10 20:40

--- abc300/b/main.cpp
+++ abc300/b/main.cpp
@@ -5,5 +5,19 @@ using ll = long long;
 
 int main() {
     // Code here
+      int h, w;
+  cin >> h >> w;
+  vector<string> a(h), b(h);
+  rep(i,h) cin >> a[i];
+  rep(i,h) cin >> b[i];
+  rep(s,h)rep(t,w) {
+    vector<string> na = a;
+    rep(i,h)rep(j,w) na[(i+s)%h][(j+t)%w] = a[i][j];
+    if (na == b) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
     return 0;
 }
